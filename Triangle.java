202914ecf5b6610a05dd1068f3b60a7c658e873a
protocols <k8328395@naver.com>
2023-07-03 12:03:42 +0900

    first commit

---
+++ Triangle.java
@@ -0,0 +1,13 @@
+
+class Triangle {
+
+	public static void main(String[] args) {
+             System.out.println("    *");	
+             System.out.println("   **");	
+             System.out.println("  ***");	
+             System.out.println(" ****");	
+             System.out.println("*****");	
+		
+
+}
+}
